66e0953281ad3d396284d1199897093de0da9d88
Jael-Vargas <jaelneftalidash@gmail.com>
2026-02-16 19:53:32 -0600

    Insert 16/02/2026

--- app.py
+++ app.py
@@ -1,46 +1,51 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask import flash
-from flask import g
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf.csrf import CSRFProtect
-
 from config import DevelopmentConfig
-import forms
 from models import db, Alumnos
+import forms
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 csrf = CSRFProtect(app)
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+
+    alumnos_list = Alumnos.query.all()
+    
+    return render_template("index.html", alumnos=alumnos_list)
 
 @app.route("/alumnos", methods=['GET', 'POST'])
 def alumnos():
-    form = forms.UserForm2()
-    alumnos_list = Alumnos.query.all()
+    create_form = forms.UserForm2(request.form)
     
-    if form.validate_on_submit():
-        alumno = Alumnos(
-            nombre=form.nombre.data,
-            apaterno=form.apaterno.data,
-            email=form.email.data
+    if request.method == 'POST' and create_form.validate():
+        alum = Alumnos(
+            nombre=create_form.nombre.data,
+            apaterno=create_form.apaterno.data,
+            amaterno=create_form.amaterno.data,
+            email=create_form.email.data
         )
-        db.session.add(alumno)
+        
+        db.session.add(alum)
         db.session.commit()
+        
         flash('Alumno registrado correctamente!')
-        return redirect(url_for('alumnos'))
+        
+        return redirect(url_for('index'))
     
-    return render_template("Alumnos.html", form=form, alumnos=alumnos_list)
+    return render_template("Alumnos.html", form=create_form)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    csrf.init_app(app)
+    db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
+        
+    app.run()
--- forms.py
+++ forms.py
@@ -3,21 +3,22 @@ from wtforms import StringField, IntegerField, PasswordField, FloatField
 from wtforms import EmailField
 from wtforms import validators
 
-
 class UserForm2(FlaskForm):
-    id=IntegerField('id',
-    [validators.number_range(min=1, max=20, message='valor no valido')])
     
-    nombre=StringField('nombre',[
+    nombre = StringField('nombre', [
         validators.DataRequired(message='El nombre es requerido'),
-        validators.length(min=4, max=20, message='requiere min=4 max=20')
+        validators.Length(min=4, max=20, message='requiere min=4 max=20')
     ])
     
-    apaterno=StringField('apaterno',[
+    apaterno = StringField('apaterno', [
         validators.DataRequired(message='El apellido es requerido')
     ])
     
-    email=EmailField('correo',[
+    amaterno = StringField('amaterno', [
+        validators.DataRequired(message='El apellido materno es requerido')
+    ])
+    
+    email = EmailField('correo', [
         validators.DataRequired(message='El correo es requerido'),
         validators.Email(message='Ingrese un correo valido')
     ])
